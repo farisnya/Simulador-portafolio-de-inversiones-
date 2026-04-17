d272cfdfc827753432795ea7392c63ed08b16fc1
Farisnya <farisnya2004@gmail.com>
2026-04-17 16:03:00 -0500

    Actualización del activo,accion y rentafija

--- main.py
+++ main.py
@@ -1,46 +1,44 @@
 import yfinance as yf
 class Activo:
-    def __init__(self, ticker, cantidad):
-        self.ticker = ticker
-        self.cantidad = cantidad
-
     def valor(self):
         raise NotImplementedError("Debe implementarse en la subclase")
-    
+
 
 class Accion(Activo):
     def __init__(self, ticker, cantidad):
         self.ticker = ticker.upper()
         self.cantidad = cantidad
-        self.data = yf.download(self.ticker, period="1y", progress=False)
+        self.precios = yf.download(self.ticker, period="1y", progress=False)
 
-    def precio_actual(self):
-        return float(self.data["Close"].iloc[-1])
+    def precioActual(self):
+        return self.precios["Close"].iloc[-1]
 
-    def precio_dia(self, index):
-        return float(self.data["Close"].iloc[index])
+    def precioDia(self, fecha):
+        return self.precios.loc[fecha]["Close"]
 
-    def valor(self, index=None):
-        if index is None:
-            return self.precio_actual() * self.cantidad
-        else:
-            return self.precio_dia(index) * self.cantidad
+    def minDia(self, fecha):
+        return self.precios.loc[fecha]["Low"]
 
-    def dividendos(self):
-        ticker = yf.Ticker(self.ticker)
-        return float(ticker.dividends.sum())
-
-
-
-class RentaFija(Activo):
-    def __init__(self, capital, tasa_anual):
-        self.capital = capital
-        self.tasa_anual = tasa_anual
-
-    def valor_actual(self, dias):
-        tasa_diaria = self.tasa_anual / 365
-        return self.capital * (1 + tasa_diaria) ** dias
+    def maxDia(self, fecha):
+        return self.precios.loc[fecha]["High"]
 
     def valor(self):
-        return self.capital
+        return self.precioActual() * self.cantidad
+
+    def obtenerDividendos(self):
+        ticker = yf.Ticker(self.ticker)
+        return ticker.dividends.sum()
+
+class RentaFija(Activo):
+    def __init__(self, capital, tasa, dias):
+        self.capital = capital
+        self.tasa = tasa
+        self.dias = dias
+
+    def valorActual(self):
+        tasa_diaria = self.tasa / 365
+        return self.capital * (1 + tasa_diaria) ** self.dias
+
+    def valor(self):
+        return self.valorActual()
     
